1d14f9729193ed56c5cd2474f5688721ed2f22e1
Cameron Givler <cameron@aqueti.com>
2016-02-04 11:52:16 -0500

    Added ThreadSafeQueue

--- test/UnitTest.cpp
+++ test/UnitTest.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #include <Timer.h>
 #include <C11Thread.h>
 #include <DataBuffer.h>
+#include <ThreadSafeQueue.h>
 
 /**
  * Main test function
@@ -26,6 +27,12 @@ int main(void)
       cout << "DataBuffer Test Failed!" << endl;
       return 1;
    }
+   if( !test_ThreadSafeQueue())
+   {
+      cout << "Threadsafe Queue Test Failed!" << endl;
+      return 1;
+   }
    
    cout << "All tests completed successfully!" << endl;
+   return 0;
 }

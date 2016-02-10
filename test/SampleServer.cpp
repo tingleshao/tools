d653cd1a64d8bb75edff5593c12e21fd72c342af
Steve Feller <steve.feller@gmail.com>
2016-02-10 14:58:32 -0500

    Added SampleServer to test socket stuff

---
+++ test/SampleServer.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+#include <Timer.h>
+#include <C11Thread.h>
+#include <DataBuffer.h>
+#include <ThreadSafeQueue.h>
+#include <ExtendedBuffer.h>
+#include <BaseSocket.h>
+#include <SocketServer.h>
+
+/**
+ * Main test function
+ **/
+int main(void)
+{
+   if( !testTimer() )
+   {
+      cout << "Timer Unit Test Failed!" << endl;
+      return 1;
+   }
+   if( !testC11Thread() )
+   {
+      cout << "C11Thread Unit Test Failed!" << endl;
+      return 1;
+   }
+   if( !testDataBuffer() )
+   {
+      cout << "DataBuffer Test Failed!" << endl;
+      return 1;
+   }
+/*
+   if( !test_ThreadSafeQueue())
+   {
+      cout << "Threadsafe Queue Test Failed!" << endl;
+   }
+   if( !testExtendedBuffer())
+   {
+      cout << "ExtendedBuffer Test Failed!" << endl;
+      return 1;
+   }
+*/
+   if( !testBaseSocket())
+   {
+      cout << "BaseSocket Test Failed!" << endl;
+      return 1;
+   }
+   if( !testSocketServer())
+   {
+      cout << "SocketServer Test Failed!" << endl;
+      return 1;
+   }
+   cout << "All tests completed successfully!" << endl;
+   return 0;
+}

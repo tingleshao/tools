f081eb7af2d7c87c781825644aece6aa568bbbe9
tingleshao <cshao@cs.unc.edu>
2016-04-18 22:50:57 +0800

    add fixed bandwidth test script.

--- test/WriteTest_tools/batch_test_fix_bandwidth.py
+++ test/WriteTest_tools/batch_test_fix_bandwidth.py
@@ -8,14 +8,14 @@ import os
 
 four_mb = 4194304
 
-for n in xrange(0, 200, 100):
+for n in xrange(0, 5000, 100):
     bandwidth = four_mb * n 
     for file_size in xrange(four_mb - 8 * 10, four_mb + 8 * 10, 8):
           number_of_files = bandwidth / file_size
-          command = './writetest -d ~/SSD/drive2/Test/fstest/ -n ' + str(number_of_files) + ' -s ' + str(file_size) + ' >> result.txt' 
+          command = './writetest -d ~/SSD/drive2/Test/fstest/ -n ' + str(number_of_files) + ' -s ' + str(file_size) + ' >> result_fixed_bandwidth_' + str(bandwidth) + '.txt' 
           print " current file size: " + str(file_size)
           print " current number of files: " + str(number_of_files)
           os.system(command) 
-    with opend("result.txt", "a") as result:
+    with open("result_fixed_bandwidth.txt", "a") as result:
         result.write("\n\n===========finished one bandwidth test\n\n")
         
